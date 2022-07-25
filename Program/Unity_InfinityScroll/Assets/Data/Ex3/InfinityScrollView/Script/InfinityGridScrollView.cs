af339297efbdf436136921f1e469583985fd47ec
BurnningGS <71960908+BurnningGS@users.noreply.github.com>
2022-07-25 19:04:15 +0900

    .
    .

--- Program/Unity_InfinityScroll/Assets/Data/Ex3/InfinityScrollView/Script/InfinityGridScrollView.cs
+++ Program/Unity_InfinityScroll/Assets/Data/Ex3/InfinityScrollView/Script/InfinityGridScrollView.cs
@@ -26,9 +26,9 @@ namespace OneP.InfinityScrollView
     public class InfinityGridScrollView : MonoBehaviour
     {
         [Header("Setting Reference object")]
-        public GameObject prefab; // link object item
-        public ScrollRect scrollRect;// link to UGUI scrollRect
-        public RectTransform content;// link to content that contain all item in scrollrect
+        public GameObject ChildItem; // link object item
+        public ScrollRect targetScrollRect;// link to UGUI scrollRect
+        public RectTransform targetContentBox;// link to content that contain all item in scrollrect
 
         [Header("Setting For Custom Scroll View")]
         public InfinityType type = InfinityType.Vertical;// type scrollview
@@ -48,155 +48,40 @@ namespace OneP.InfinityScrollView
         public bool isOverrideSettingScrollbar = true;
         public bool setupOnAwake = true;
 
-        private List<GameObject> listItem = new List<GameObject>();
+        private List<GameObject> ChildLst = new List<GameObject>();
         private GameObject[] arrayCurrent = null;
         private int cacheOld = -1;
         private bool isInit = false;
 
 
-        public void Setup(int numberItem)
-        {
-            totalNumberItem = numberItem;
-            if (totalNumberItem < 0)
-            {
-                totalNumberItem = 0;
-            }
-            Setup();
-        }
-
-        public void Setup()
-        {
-            if (prefab == null)
-            {
-                Debug.LogWarning("No prefab/Gameobject Item linking");
-                return;
-            }
-            if (type == InfinityType.Vertical)
-            {
-                int num = Mathf.CeilToInt(totalNumberItem / showCellNumber.x);
-                int totalHeight = (int)(num * cellSize.y);
-                content.SetHeight(totalHeight);
-            }
-            else
-            {
-                int num = Mathf.CeilToInt(totalNumberItem / showCellNumber.y);
-                int totalWidth = (int)(num * cellSize.x);
-                content.SetWidth(totalWidth);
-            }
-            //reset Array
-            arrayCurrent = new GameObject[totalNumberItem];
-
-            int itemX = horizontalType == HorizontalType.LeftToRight ? 0 : 1;
-            int itemY = verticalType == VerticalType.TopToBottom ? 1 : 0;
-            content.anchorMin = new Vector2(itemX, itemY);
-            content.anchorMax = new Vector2(itemX, itemY);
-            content.pivot = new Vector2(itemX, itemY);
-
-            for (int i = 0; i < itemGenerate; i++)
-            {
-                GameObject obj = null;
-                if (!isInit)
-                {
-                    if (i < totalNumberItem)
-                    {
-                        obj = GameObject.Instantiate(prefab) as GameObject;
-                        obj.name = "item_" + (i);
-                        obj.transform.SetParent(content.transform, false);
-                        obj.transform.localScale = Vector3.one;
-                        listItem.Add(obj);
-                        RectTransform rect = obj.GetComponent<RectTransform>();
-                        if (rect != null)
-                        {
-                            rect.anchorMin = new Vector2(itemX, itemY);
-                            rect.anchorMax = new Vector2(itemX, itemY);
-                            rect.pivot = new Vector2(itemX, itemY);
-                        }
-                        Reload(obj, i);
-                        arrayCurrent[i] = obj;
-                    }
-                }
-                else
-                {
-                    if (i < totalNumberItem)
-                    {
-                        obj = listItem[i];
-                        obj.SetActive(true);
-                        Reload(obj, i);
-                        arrayCurrent[i] = obj;
-                    }
-                    else
-                    {
-                        obj = listItem[i];
-                        obj.SetActive(false);
-                    }
-                }
-            }
-            isInit = true;
-        }
-        private float GetContentSize()
-        {
-            return content.GetHeight();
-        }
+        //============================================================================
+        //
+        // Unity Base
+        //
 
         void Awake()
         {
-            scrollRect = GetComponent<ScrollRect>();
-            scrollRect.onValueChanged.AddListener(OnScrollChange);
+            // 이벤트 추가
+            targetScrollRect = GetComponent<ScrollRect>();
+            targetScrollRect.onValueChanged.AddListener(Event_OnScrollChange);
+            
+            // 셋업(초기화)
             if (setupOnAwake)
-            {
                 Setup();
-            }
         }
 
-        private int GetCurrentIndex()
-        {
-            int index = -1;
-            if (type == InfinityType.Vertical)
-            {
-                if (verticalType == VerticalType.TopToBottom)
-                {
-                    index = (int)(content.anchoredPosition.y / cellSize.y);
-                }
-                else
-                {
-                    index = (int)(-content.anchoredPosition.y / cellSize.y);
-                }
-            }
-            else
-            {
-                if (horizontalType == HorizontalType.LeftToRight)
-                {
-                    index = (int)(-content.anchoredPosition.x / cellSize.x);
 
-                }
-                else
-                {
-                    index = (int)(content.anchoredPosition.x / cellSize.x);
-                }
-            }
-            if (index < 0)
-                index = 0;
-            int maxNum = type == InfinityType.Vertical ? (int)showCellNumber.x : (int)showCellNumber.y;
-            int ceil = Mathf.CeilToInt(totalNumberItem / maxNum);
-            if (index > ceil - 1)
-            {
-                index = ceil - 1;
-            }
-            //Debug.LogError("test:" + index);
-            return index;
-        }
-        public void InternalReload()
-        {
+        //============================================================================
+        //
+        // Event
+        //
 
-            int index = GetCurrentIndex();
-            FixFastReload(index);
-        }
-        public void OnScrollChange(Vector2 vec)
+        #region [Evnet] OnScrollChange
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        public void Event_OnScrollChange(Vector2 vec)
         {
             if (arrayCurrent == null || arrayCurrent.Length < 1)
-            {
                 return;
-            }
 
             int index = GetCurrentIndex();
             if (cacheOld != index)
@@ -218,7 +103,140 @@ namespace OneP.InfinityScrollView
                 }
             }
         }
+        #endregion
 
+
+        //============================================================================
+        //
+        // 초기화
+        //
+
+        public void Setup()
+        {
+            //------------------------------------------------------
+            // 예외처리
+            //
+            if (ChildItem == null)
+            {
+                Debug.LogWarning("No prefab/Gameobject Item linking");
+                return;
+            }
+
+            //------------------------------------------------------
+            // 총 너비 또는 높이 계산
+            //
+            if (type == InfinityType.Vertical) // 세로방향일때 총 높이 계산
+            {
+                int num = Mathf.CeilToInt(totalNumberItem / showCellNumber.x); 
+                int totalHeight = (int)(num * cellSize.y);
+                targetContentBox.SetHeight(totalHeight);
+            }
+            else if (type == InfinityType.Horizontal) // 가로 방향일떄 총 너비 계산.
+            {
+                int num = Mathf.CeilToInt(totalNumberItem / showCellNumber.y);
+                int totalWidth = (int)(num * cellSize.x);
+                targetContentBox.SetWidth(totalWidth);
+            }
+
+            //------------------------------------------------------
+            // 초기화
+            //
+
+            // 배열 초기화
+            arrayCurrent = new GameObject[totalNumberItem];
+
+            // 앵커 % 피봇 초기화.
+            int itemX = horizontalType == HorizontalType.LeftToRight ? 0 : 1;
+            int itemY = verticalType == VerticalType.TopToBottom ? 1 : 0;
+            targetContentBox.anchorMin = new Vector2(itemX, itemY);
+            targetContentBox.anchorMax = new Vector2(itemX, itemY);
+            targetContentBox.pivot = new Vector2(itemX, itemY);
+
+
+            // 아이템 생성. 
+            for (int i = 0; i < itemGenerate; i++)
+            {
+                GameObject obj;
+                if (!isInit)
+                {
+                    if (i < totalNumberItem)
+                    {
+                        obj = Instantiate(ChildItem);
+                        obj.name = "item_" + (i); // 이름 설정
+                        obj.transform.SetParent(targetContentBox.transform, false); // 부모 설정. 
+                        obj.transform.localScale = Vector3.one;
+                        ChildLst.Add(obj);
+                        RectTransform rect = obj.GetComponent<RectTransform>();
+                        if (rect != null)
+                        {
+                            rect.anchorMin = new Vector2(itemX, itemY);
+                            rect.anchorMax = new Vector2(itemX, itemY);
+                            rect.pivot = new Vector2(itemX, itemY);
+                        }
+                        Reload(obj, i);
+                        arrayCurrent[i] = obj;
+                    }
+                }
+                else
+                {
+                    if (i < totalNumberItem)
+                    {
+                        obj = ChildLst[i];
+                        obj.SetActive(true);
+                        Reload(obj, i);
+                        arrayCurrent[i] = obj;
+                    }
+                    else
+                    {
+                        obj = ChildLst[i];
+                        obj.SetActive(false);
+                    }
+                }
+            }
+            isInit = true;
+        }
+
+        #region [파악중]
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        private int GetCurrentIndex()
+        {
+            int index = -1;
+
+            if (type == InfinityType.Vertical)
+            {
+                if (verticalType == VerticalType.TopToBottom)
+                    index = (int)(targetContentBox.anchoredPosition.y / cellSize.y);
+                else
+                    index = (int)(-targetContentBox.anchoredPosition.y / cellSize.y);
+            }
+
+            else if (type == InfinityType.Horizontal)
+            {
+                if (horizontalType == HorizontalType.LeftToRight)
+                    index = (int)(-targetContentBox.anchoredPosition.x / cellSize.x);
+                else
+                    index = (int)(targetContentBox.anchoredPosition.x / cellSize.x);
+            }
+
+            if (index < 0)
+                index = 0;
+            
+            int maxNum = type == InfinityType.Vertical ? (int)showCellNumber.x : (int)showCellNumber.y;
+            
+            int ceil = Mathf.CeilToInt(totalNumberItem / maxNum);
+            
+            if (index > ceil - 1)
+                index = ceil - 1;
+            
+
+            return index;
+        }
+        public void InternalReload()
+        {
+
+            int index = GetCurrentIndex();
+            FixFastReload(index);
+        }
         private void ChangeReload(int index)
         {
             GameObject objIndex = arrayCurrent[index];
@@ -311,7 +329,7 @@ namespace OneP.InfinityScrollView
                 //Debug.LogError ("Fix Fast reload:"+start+","+index);
                 for (int i = 0; i < itemGenerate; i++)
                 {
-                    arrayCurrent[start + i] = listItem[i];
+                    arrayCurrent[start + i] = ChildLst[i];
                     Reload(arrayCurrent[start + i], start + i);
                 }
                 return true;
@@ -326,51 +344,52 @@ namespace OneP.InfinityScrollView
             vec = new Vector2(overrideX, overrideY);
             vec = GetLocationAppear(vec, indexReload);
             obj.transform.localPosition = vec;
+         
             InfinityBaseItem baseItem = obj.GetComponent<InfinityBaseItem>();
             if (baseItem != null)
-            {
                 baseItem.Reload(indexReload);
-            }
         }
 
-        private Vector3 GetLocationAppear(Vector2 initVec, int location)
+        // Content 내 Local 위치 지정. 
+        private Vector3 GetLocationAppear(Vector2 offset, int location)
         {
-            Vector3 vec = initVec;
-            if (type == InfinityType.Vertical)
+            Vector3 vec = offset;
+
+            if (type == InfinityType.Vertical) // 세로형 타입일때.
             {
                 if (verticalType == VerticalType.TopToBottom)
-                {
                     vec = new Vector3(vec.x + cellSize.x * (location % showCellNumber.x), -cellSize.y * (location / (int)showCellNumber.x), 0);
-                }
+             
                 else
-                {
                     vec = new Vector3(vec.x + cellSize.x * (location % showCellNumber.x), cellSize.y * (location / (int)showCellNumber.x), 0);
-                }
             }
-            else
+
+            else if (type == InfinityType.Horizontal) // 세로형 타입일때.
             {
                 float x;
                 float y;
+
                 if (horizontalType == HorizontalType.LeftToRight)
-                {
                     x = cellSize.x * (location / (int)showCellNumber.y);
-                }
+                
                 else
-                {
                     x = -cellSize.x * (location / (int)showCellNumber.y);
-                }
+
 
                 if (verticalType == VerticalType.TopToBottom)
-                {
                     y = -(vec.y + cellSize.y * (location % showCellNumber.y));
-                }
+                
                 else
-                {
                     y = vec.y + cellSize.y * (location % showCellNumber.y);
-                }
+                
                 vec = new Vector3(x, y, 0);
             }
+
             return vec;
         }
+
+        #endregion
+
+
     }
 }
